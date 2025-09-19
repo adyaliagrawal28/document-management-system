2e12d7a39b742f86af8d81136fbacc2848770f37
Adyali Agrawal <adyaliagrawal28@gmail.com>
2025-09-19 19:04:34 +0530

    Made navbar responsive with collapse and toggle button and changed the color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,30 @@
 import { Link } from "react-router-dom";
-import { Navbar as BsNavbar, Container, Nav } from "react-bootstrap";
+import { Navbar as BsNavbar, Container, Nav, NavDropdown } from "react-bootstrap";
 
 function Navbar() {
   return (
-    <BsNavbar bg="dark" variant="dark" expand="lg">
-      <Container>
+   <BsNavbar style={{ backgroundColor: "#561ba3ff" }} variant="dark" expand="lg" collapseOnSelect>
+      <Container fluid>
         <BsNavbar.Brand as={Link} to="/">
           Document Manager
         </BsNavbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/">
-            Login
-          </Nav.Link>
-          <Nav.Link as={Link} to="/admin">
-            Admin
-          </Nav.Link>
-          <Nav.Link as={Link} to="/upload">
-            Upload
-          </Nav.Link>
-          <Nav.Link as={Link} to="/search">
-            Search
-          </Nav.Link>
-        </Nav>
+        <BsNavbar.Toggle aria-controls="basic-navbar-nav" />
+        <BsNavbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to="/admin">
+              Admin
+            </Nav.Link>
+            <Nav.Link as={Link} to="/upload">
+              Upload
+            </Nav.Link>
+            <Nav.Link as={Link} to="/search">
+              Search
+            </Nav.Link>
+          </Nav>
+        </BsNavbar.Collapse>
       </Container>
     </BsNavbar>
   );
